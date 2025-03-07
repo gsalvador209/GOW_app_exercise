2114a5a6cbd6c19ca28386b384e74ca2d2a30ecf
Rafael <rafael.glez@gmail.com>
2025-03-07 15:39:18 -0600

    Class version

---
+++ GOW/View/WeaponsCell.swift
@@ -0,0 +1,23 @@
+//
+//  WeaponsCell.swift
+//  GOW
+//
+//  Created by Rafael Gonzalez on 07/03/25.
+//
+
+import UIKit
+
+class WeaponsCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
